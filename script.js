ff77725020e6966b4b61fa603572bf926720eb5d
VKS1572 <vikaspradhanrisora@gmail.com>
2026-07-06 00:02:12 +0530

    new update: Added a playbar at the bottom of the page with song buttons for next and previous songs. Updated CSS to style the playbar and song buttons.

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 console.log("Hello World");
+
+async function getSongs() {
+   let a = await fetch("http://127.0.0.1:3000/songs");
+   let response = await a.text();
+   let div = document.createElement("div");
+   div.innerHTML = response;
+   let as = div.getElementsByTagName("a");
+   let songs = [];
+   
+   for(let index = 0; index < as.length; index++){
+      const element = as[index];
+      if(element.href.endsWith(".mp3")){
+        songs.push(element.href);
+      }
+   }
+   return songs;
+}
+
+async function main() {
+    //list all the songs in the console
+let songs = await getSongs();
+console.log(songs);
+
+//play the first song
+let audio = new Audio(songs[0]);
+audio.play();
+}
+
+main()
