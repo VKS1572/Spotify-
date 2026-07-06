c131c88e0373578f0a86eef87e4f1a0e6ed08736
VKS1572 <vikaspradhanrisora@gmail.com>
2026-07-06 18:30:28 +0530

    new updates

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 console.log("Hello World");
 
+let currentSong = new Audio;
+
 async function getSongs() {
-   let a = await fetch("http://127.0.0.1:3000/songs");
+   let a = await fetch("http://127.0.0.1:3000/songs/");
    let response = await a.text();
    let div = document.createElement("div");
    div.innerHTML = response;
@@ -10,21 +12,48 @@ async function getSongs() {
    
    for(let index = 0; index < as.length; index++){
       const element = as[index];
+
       if(element.href.endsWith(".mp3")){
-        songs.push(element.href);
+      songs.push(decodeURIComponent(element.href.split("/").pop()));
       }
    }
-   return songs;
+   return songs
+}
+
+
+const playMusic = (track)=>{
+   //let audio = new Audio("/songs/" + track);
+   currentSong.src = "/songs/"+ track
+   currentSong.play()
 }
 
 async function main() {
-    //list all the songs in the console
+    //get list all the songs in the console
 let songs = await getSongs();
-console.log(songs);
 
-//play the first song
-let audio = new Audio(songs[0]);
-audio.play();
+
+//show all the songs in the playlist
+let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
+for (const song of songs) {
+   songUL.innerHTML = songUL.innerHTML +  `<li>
+    
+                <img class="invert" width="34" src="music.svg" alt="">
+                <div class="info">
+                  <div > ${song.replaceAll("%20", " ")}</div>
+                  <div>Vikas</div>
+                </div>
+                <div class="playnow"> 
+                   <span>Play Now</span>
+                   <img class= "invert" src="play.svg" alt="">
+                </div> </li>`;
+   
+}
+Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e=>{
+console.log(e.querySelector(".info").firstElementChild.innerHTML)
+playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
+
+})
+
 }
 
 main()
