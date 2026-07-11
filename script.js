fdb3daad091f72fb25e87a5677b500ebcd9d0c55
VKS1572 <vikaspradhanrisora@gmail.com>
2026-07-11 17:12:43 +0530

    playing music one after other fun

--- script.js
+++ script.js
@@ -1,59 +1,124 @@
 console.log("Hello World");
 
-let currentSong = new Audio;
+let currentSong = new Audio();
+let songs = [];
 
+// Get all songs
 async function getSongs() {
-   let a = await fetch("http://127.0.0.1:3000/songs/");
-   let response = await a.text();
-   let div = document.createElement("div");
-   div.innerHTML = response;
-   let as = div.getElementsByTagName("a");
-   let songs = [];
-   
-   for(let index = 0; index < as.length; index++){
-      const element = as[index];
+    let a = await fetch("http://127.0.0.1:3000/songs/");
+    let response = await a.text();
 
-      if(element.href.endsWith(".mp3")){
-      songs.push(decodeURIComponent(element.href.split("/").pop()));
-      }
-   }
-   return songs
+    let div = document.createElement("div");
+    div.innerHTML = response;
+
+    let as = div.getElementsByTagName("a");
+    let songs = [];
+
+    for (let i = 0; i < as.length; i++) {
+        let href = as[i].getAttribute("href");
+
+        if (href && href.endsWith(".mp3")) {
+
+            // Remove folders and decode filename
+            let song = decodeURIComponent(href);
+
+            // Remove Windows or Linux path
+            song = song.split("\\").pop();
+            song = song.split("/").pop();
+
+            songs.push(song);
+        }
+    }
+
+    return songs;
 }
 
+// Play music
+function playMusic(track) {
 
-const playMusic = (track)=>{
-   //let audio = new Audio("/songs/" + track);
-   currentSong.src = "/songs/"+ track
-   currentSong.play()
+    track = track.split("\\").pop();
+    track = track.split("/").pop();
+
+    currentSong.src =
+        "http://127.0.0.1:3000/songs/" + encodeURIComponent(track);
+
+    console.log("Playing URL:", currentSong.src);
+
+    document.querySelector(".songinfo").innerHTML = track;
+
+    currentSong.play()
+        .then(() => {
+            document.getElementById("play").src = "pause.svg";
+        })
+        .catch(err => {
+            console.error(err);
+        });
 }
 
 async function main() {
-    //get list all the songs in the console
-let songs = await getSongs();
 
+    songs = await getSongs();
 
-//show all the songs in the playlist
-let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
-for (const song of songs) {
-   songUL.innerHTML = songUL.innerHTML +  `<li>
-    
-                <img class="invert" width="34" src="music.svg" alt="">
-                <div class="info">
-                  <div > ${song.replaceAll("%20", " ")}</div>
-                  <div>Vikas</div>
-                </div>
-                <div class="playnow"> 
-                   <span>Play Now</span>
-                   <img class= "invert" src="play.svg" alt="">
-                </div> </li>`;
-   
+    console.log(songs);
+
+    let songUL = document.querySelector(".songList ul");
+    songUL.innerHTML = "";
+
+    for (const song of songs) {
+
+        songUL.innerHTML += `
+        <li>
+            <img class="invert" width="34" src="music.svg">
+
+            <div class="info">
+                <div>${song}</div>
+                <div>Vikas</div>
+            </div>
+
+            <div class="playnow">
+                <span>Play Now</span>
+                <img class="invert" src="play.svg">
+            </div>
+        </li>`;
+    }
+
+    Array.from(document.querySelectorAll(".songList li")).forEach((e) => {
+
+        e.addEventListener("click", () => {
+
+            let track = e.querySelector(".info div").innerText.trim();
+
+            playMusic(track);
+
+        });
+
+    });
+
+    // Play / Pause
+    document.getElementById("play").addEventListener("click", () => {
+
+        if (currentSong.paused) {
+            currentSong.play();
+            play.src = "pause.svg";
+        } else {
+            currentSong.pause();
+            play.src = "play.svg";
+        }
+
+    });
+
+    // Update time
+    currentSong.addEventListener("timeupdate", () => {
+
+        document.querySelector(".songtime").innerHTML =
+            `${Math.floor(currentSong.currentTime)} / ${Math.floor(currentSong.duration)}`;
+
+    });
+
+    currentSong.addEventListener("error", (e) => {
+        console.log("Audio Error:", e);
+    });
+
 }
-Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e=>{
-console.log(e.querySelector(".info").firstElementChild.innerHTML)
-playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
 
-})
-
-}
-
-main()
+main();
